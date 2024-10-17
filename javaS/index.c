4a4b44a80492f94fdc3a9030cbe4bdcbc02d5be5
ADE-SHILE <olubidoridwan@gmail.com>
2024-10-17 17:42:43 +0100

    first commit

---
+++ javaS/index.c
@@ -0,0 +1,6 @@
+int x = 5
+
+// char name = "a"
+char a = "ridwan"
+
+printf(x)
---
+++ javaS/index.js
@@ -0,0 +1,10 @@
+// let x = 5 // integer
+
+// let man = "ridwan" // string
+
+// console.log(x)
+
+// alert("Hello everyone")
+
+// let name = prompt("What is your name?")
+console.log('Hello World')
